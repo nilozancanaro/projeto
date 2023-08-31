92699cc7c6c2b3b7a9b63b5cb0780602f12c6f20
nilozancanaro <nilozancanaro@hotmail.com>
2023-08-31 16:41:01 -0300

    script.js

--- assets/script.js
+++ assets/script.js
@@ -10,9 +10,9 @@ function toggleMode() {
   // pegar a tag img
   const img = document.querySelector("#profile img")
   // substituir a imagem
-    if (html.classList.contains("light")) {
-  // se tiver light mode, add a imagem light
-      img.setAttribute("src", "./assets/avatar-light.png")
+  if (html.classList.contains("light")) {
+    // se tiver light mode, add a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
